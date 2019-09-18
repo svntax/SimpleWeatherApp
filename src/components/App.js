d3590e7f3b76e7ff5ddff9f12830012cabb1ff6e
svntax <endless.answers@gmail.com>
2019-09-18 01:46:50 -0400

    Basic setup

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./App.css";
+
+class App extends React.Component {
+	
+	constructor(props){
+		super(props);
+	}
+	
+	render(){
+		return (
+			<div className="app">
+				<h1>Simple Weather App</h1>
+			</div>
+		);
+	}
+	
+}
+
+export default App;
